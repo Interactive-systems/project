98722e12ea610493f7bb9d9ac25a7f298b1c5006
Miika Sirén <mssiren@cs.helsinki.fi>
2013-02-12 15:47:50 +0200

    Made the map mode the default view of the app. Removed the physical buttons and moved them to the menu.

--- src/com/inter_act/interactivesystems/MainActivity.java
+++ src/com/inter_act/interactivesystems/MainActivity.java
@@ -1,55 +1,107 @@
 package com.inter_act.interactivesystems;
 
-import android.app.Activity;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
-import android.view.View;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends FragmentActivity implements LocationListener {
+
+	GoogleMap googleMap;
+	private LocationManager mlocManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.map);
 
 		//Setting context to use in any environment
 		DataHolder.getDataHolderObject().setContext(this);
+
+		mlocManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+		mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+
+		//Getting Google Play Services availability status
+		int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
+
+		//Showing the status
+		if (status != ConnectionResult.SUCCESS) { // Google Play Services are not available
+			int requestCode = 10;
+			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+			dialog.show();
+		} 
+		else { //Google Play Services are available
+
+			// Getting reference to the SupportMapFragment of map.xml
+			SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+			googleMap = fm.getMap();
+			googleMap.setMyLocationEnabled(true);
+		}
 	}
 
-	public void mapModeButtonClicked(View view) {
+	public void locationChanged(Location location) {
+		TextView tvLocation = (TextView) findViewById(R.id.tv_location);
+		double latitude = location.getLatitude();
+		double longitude = location.getLongitude();
+		LatLng latLng = new LatLng(latitude, longitude);
+
+		googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+		googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+		tvLocation.setText("Latitude:" +  latitude  + ", Longitude:"+ longitude );
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		if (DataHolder.isBetterLocation(location, DataHolder.getDataHolderObject().getCurrentBestLocation())) {
+			DataHolder.getDataHolderObject().setCurrentBestLocation(location);
+
+			Log.d("test", "isBetterLocation");
+		}
+		else {
+			location = DataHolder.getDataHolderObject().getCurrentBestLocation();
+			Log.d("test", "isNotBetterLocation");
+		}
+
+		locationChanged(location);
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {	
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+	}
+
+	/*
+	public void mapModeButtonClicked(View view) {	
 		Log.d("test", "map mode button clicked");
 		Intent mapMode = new Intent(MainActivity.this, CustomMapActivity.class);
 		MainActivity.this.startActivity(mapMode);
 	}
-
-	public void drawModeButtonClicked(View view) {
-		//Intent drawMode = new Intent(this, DrawActivity.class);
-		//this.startActivity(drawMode);
-		Log.d("test", "draw mode button clicked");
-	}
-
-	public void galleryModeButtonClicked(View view) {
-		//Intent galleryMode = new Intent(this, GalleryActivity.class);
-		//this.startActivity(galleryMode);
-		Log.d("test", "gallery mode button clicked");
-	}
-	
-	public void cameraModeButtonClicked(View view) {
-		ProgressDialog pd = new ProgressDialog(this);
-		pd.setTitle("Downloading image");
-		pd.setCancelable(false);
-		pd.setCanceledOnTouchOutside(false);
-		pd.show();
-		DataHolder.getDataHolderObject().setProgressDialog(pd);
-		
-		Intent cameraMode = new Intent(this, CameraPreviewActivity.class);
-		cameraMode.putExtra("url", "http://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/PNG_transparency_demonstration_2.png/300px-PNG_transparency_demonstration_2.png");
-		cameraMode.putExtra("direction", "S");
-		this.startActivity(cameraMode);
-	}
+	 */
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,13 +111,38 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
-	public void onResume() {
-		super.onResume();
-	}
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
 
-	@Override
-	public void onDestroy(){
+		case R.id.action_camera_mode:
+			ProgressDialog pd = new ProgressDialog(this);
+			pd.setTitle("Downloading image");
+			pd.setCancelable(false);
+			pd.setCanceledOnTouchOutside(false);
+			pd.show();
+			DataHolder.getDataHolderObject().setProgressDialog(pd);
+
+			Intent cameraMode = new Intent(this, CameraPreviewActivity.class);
+			cameraMode.putExtra("url", "http://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/PNG_transparency_demonstration_2.png/300px-PNG_transparency_demonstration_2.png");
+			cameraMode.putExtra("direction", "S");
+			this.startActivity(cameraMode);
+			break;
+		case R.id.action_draw_mode:
+			//Intent drawMode = new Intent(this, DrawActivity.class);
+			//this.startActivity(drawMode);
+			Log.d("test", "draw mode button clicked");
+			break;
+		case R.id.action_gallery_mode:
+			//Intent galleryMode = new Intent(this, GalleryActivity.class);
+			//this.startActivity(galleryMode);
+			Log.d("test", "gallery mode button clicked");
+			break;
+		}
+		return true;
+	}
+			
+	public void onDestroy() {
+		mlocManager.removeUpdates(this);
 		super.onDestroy();
 	}
-
 } 
